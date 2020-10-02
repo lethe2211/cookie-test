27125fae15e34cec646729dd78345244b7784555
lethe2211 <lethe2211@gmail.com>
2020-10-02 13:40:23 +0900

    Add a sample

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const app = express()
+const port = 3000
+
+app.use(cookieParser())
+
+app.get('/', (req, res) => {
+  console.log(req.cookies)
+
+  // Sample Cookies 
+  const cookieOption = {
+    httpOnly: true
+  }
+  res.cookie('key', 'value', cookieOption)
+  res.cookie('key2', 'value2')
+
+  res.send(`Cookie: ${res.get('Set-Cookie')}`) 
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
